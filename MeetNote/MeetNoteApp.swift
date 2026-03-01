12f8818eaa3581e0338b120f9ae199469c71d686
Theagentvikram <theagentvikram@gmail.com>
2026-03-01 10:20:42 +0530

    Initial Commit

---
+++ MeetNote/MeetNoteApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetNoteApp.swift
+//  MeetNote
+//
+//  Created by AMMUAARU on 01/03/26.
+//
+
+import SwiftUI
+
+@main
+struct MeetNoteApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MeetNoteTests/MeetNoteTests.swift
@@ -0,0 +1,16 @@
+//
+//  MeetNoteTests.swift
+//  MeetNoteTests
+//
+//  Created by AMMUAARU on 01/03/26.
+//
+
+import Testing
+
+struct MeetNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
